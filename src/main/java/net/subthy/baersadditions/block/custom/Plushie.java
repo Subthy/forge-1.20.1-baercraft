76f06378bbfd7f23580fb5790481efe4afd64742
Kristers <kristers891@gmail.com>
2023-09-10 11:30:16 +0300

    Added sound when right clicked to plushies

--- src/main/java/net/subthy/baersadditions/block/custom/Plushie.java
+++ src/main/java/net/subthy/baersadditions/block/custom/Plushie.java
@@ -1,17 +1,27 @@
 package net.subthy.baersadditions.block.custom;
 
+import net.minecraft.client.resources.sounds.SoundEventRegistration;
 import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraft.world.phys.shapes.CollisionContext;
+import net.subthy.baersadditions.sound.ModSounds;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.stream.Stream;
@@ -52,6 +62,16 @@ public class Plushie extends HorizontalDirectionalBlock {
             case EAST -> shapeE;
             default -> shapeN;
         };
+    }
+
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos,
+                                 Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        if(!pLevel.isClientSide() && pHand == InteractionHand.MAIN_HAND) {
+            pLevel.playSound(null, pPos, ModSounds.Plushie_Sound.get(), SoundSource.BLOCKS, 1f, 1f);
+            return InteractionResult.SUCCESS;
+        }
+        return super.use(pState, pLevel, pPos, pPlayer, pHand, pHit);
     }
 
     @Nullable
